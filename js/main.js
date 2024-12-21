bf9f8f8b3b19131ff4296770411e2cf5ec5c4375
sedra-bazt <baztsedra@gmail.com>
2024-12-21 17:50:18 +0200

    upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// let box=document.querySelectorAll(".work .box ")
+// console.log(box)
+
+// box.forEach((x)=>{
+//     x.addEventListener("onclick",(e)=>{
+//         x.classList.add("car")
+//     })
+// })
